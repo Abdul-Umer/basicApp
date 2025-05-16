2bc5599e532c42d8660758540c13e020766925f5
Abdul-Umer <abdulumer1205@gmail.com>
2025-05-16 19:38:40 +0530

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
+import os
+
 app=Flask(__name__)
 
 @app.route('/')
 def home():
     return "<h1> Hello</h1>"
 
+@app.route('/about')
+def about():
+    return "<h3> This is Umer a Python Dev</h3>"
 
 if __name__=="__main__":
+    
     app.run(debug=True)
